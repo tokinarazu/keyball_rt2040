135eba8f675d5b32152ae16fcb02e18166a4d986
tokinarazu <tokinarazu@gmail.com>
2025-02-02 23:20:09 +0900

    マウスキーを押すまで Auto Mouse Layer に留まる

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/tokinarazu_rp2040/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/tokinarazu_rp2040/keymap.c
@@ -168,6 +168,9 @@ color.h
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
+    keyball_keep_auto_mouse_layer_if_needed(state);
+#endif
 
     uint8_t layer = biton32(state);
     switch (layer) {
